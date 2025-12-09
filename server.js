70f32edea67e90b1ab6219c6f637d113c041a02d
adil-bmdu <adil.bmdu@gmail.com>
2025-12-09 09:22:26 +0000

    cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
+app.use(cors());
 
 // Middleware
 app.use(express.json());
